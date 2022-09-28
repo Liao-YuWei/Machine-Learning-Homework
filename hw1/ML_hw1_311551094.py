ad047782444f211f0977b1603deed8eac1cd4c6e
unknown <awei6209@gmail.com>
2022-09-28 20:12:53 +0800

    finish matrix multiplication and addition

--- hw1/ML_hw1_311551094.py
+++ hw1/ML_hw1_311551094.py
@@ -28,25 +28,44 @@ def transpose(A):
             AT[row][col] = A[col][row]
     return AT
 
+def matrixMulti(A, B):
+    try:
+        num_row, num_col = A.shape[0], B.shape[1]
+        result = np.zeros((num_col, num_row))
+        for row in range(num_row):
+            for col in range(num_col):
+                for i in range(A.shape[1]):
+                    result[row][col] += A[row][i] * B[i][col]
+    except IndexError:
+        print('IndexError at matrixMulti, the size of 2 matrices may not match')
+    return result
 
+
+def matrixAdd(A, B):
+    try:
+        num_row, num_col = A.shape
+        result = np.zeros((num_col, num_row))
+        for row in range(num_row):
+            for col in range(num_col):
+                result[row][col] = A[row][col] + B[row][col]
+    except IndexError:
+        print('IndexError at matrixAdd, the size of 2 matrices may not match')
+    return result
 
 
 file_path = input('Please enter file path and names: ')
-n = int(input('Please enter the number of  polynomial bases: '))
+n = int(input('Please enter the number of polynomial bases: '))
 lambda_ = float(input('Please enter lambda: '))
 
 A, b = generateAb(file_path, n)
 
-#print(A)
-#print(b)
-
 AT = transpose(A)
-#print(AT)
 
 """
 LSE
 """
-
+lambdaI = lambda_ * np.identity(A.shape[1])
+x = matrixAdd(matrixMulti(AT, A), lambdaI)
 
 
 
