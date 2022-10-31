ede602e4e6d6272ba6197a14b1e6850464168a1b
YuWei Liao <awei6209@gmail.com>
2022-10-31 12:04:12 +0800

    finish sequential estimator

--- hw3/ML_hw3_sequential_estimator.py
+++ hw3/ML_hw3_sequential_estimator.py
@@ -1,3 +1,4 @@
+from operator import truth
 import numpy as np
 
 def random_data_generator(m, s):
@@ -6,6 +7,23 @@ def random_data_generator(m, s):
     offset = m - original_mean
     return sum(np.random.uniform(0, 1, n)) + offset 
 
-mean = int(input('Enter the mean of gaussian for univariate gaussian data generator: '))
-variance = int(input('Enter the variance of gaussian for univariate gaussian data generator: '))
+truth_mean = int(input('Enter the mean of gaussian for univariate gaussian data generator: '))
+truth_variance = int(input('Enter the variance of gaussian for univariate gaussian data generator: '))
 
+print(f'Data point source function: N({truth_mean}, {truth_variance})', end = '\n\n')
+
+n = 0
+predict_mean = 0
+predict_variance = 0
+
+while True:
+    new_data = random_data_generator(truth_mean, truth_variance)
+    print(f'Add data point: {new_data}')
+    n += 1
+    predict_mean = (predict_mean * (n-1) + new_data) / n
+    sqr_error = (new_data - predict_mean) ** 2
+    predict_variance = (predict_variance * (n-1) + sqr_error) / n
+    print(f'Mean = {predict_mean}   Variance = {predict_variance}', end = '\n\n')
+
+    if abs(truth_mean - predict_mean) < 5e-2 and abs(truth_variance - predict_variance) < 5e-2:
+        break
