9fd00d29c01634b7b3320f26ee16c59a4c5e79d2
YuWei Liao <awei6209@gmail.com>
2022-11-29 22:24:53 +0800

    GP optimized by maximizing marginal likelihood

--- hw5/ML_hw5_GP.py
+++ hw5/ML_hw5_GP.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy import matmul as mul
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 def loadData(filepath):
     X = np.zeros((34))
@@ -35,8 +36,8 @@ def GP_predict(X, Y, covariance_matrix, beta, predict_sample_size, alpha, length
     num_data = X.shape[0]
     mean = np.zeros(predict_sample_size)
     variance = np.zeros(predict_sample_size)
-    x_sample_min = np.min(X) - abs(np.min(X)) * 0.15
-    x_sample_max = np.max(X) + abs(np.max(X)) * 0.15
+    x_sample_min = np.min(X) - abs(np.min(X)) * 0.2
+    x_sample_max = np.max(X) + abs(np.max(X)) * 0.2
     x_sample = np.linspace(x_sample_min, x_sample_max, predict_sample_size)
 
     for sample in range(predict_sample_size):
@@ -50,8 +51,8 @@ def GP_predict(X, Y, covariance_matrix, beta, predict_sample_size, alpha, length
     return mean, variance
 
 def GP_plot(X, Y, mean, variance, predict_sample_size):
-    x_sample_min = np.min(X) - abs(np.min(X)) * 0.15
-    x_sample_max = np.max(X) + abs(np.max(X)) * 0.15
+    x_sample_min = np.min(X) - abs(np.min(X)) * 0.2
+    x_sample_max = np.max(X) + abs(np.max(X)) * 0.2
     x_sample = np.linspace(x_sample_min, x_sample_max, predict_sample_size)
     interval = 1.96 * (variance ** 0.5)
 
@@ -66,6 +67,19 @@ def GP_plot(X, Y, mean, variance, predict_sample_size):
 
     return
 
+def negative_marginal_log_likelihood(theta, X, Y, beta):
+    alpha = theta[0]
+    lengthscale = theta[1]
+    kernel_variance = theta[2]
+    n = X.shape[0]
+
+    covariance_matrix_theta = create_covariance_matrix(X, beta, alpha, lengthscale, kernel_variance)
+    likelihood = np.log(np.linalg.det(covariance_matrix_theta)) / 2
+    likelihood += mul(mul(Y.T, inv(covariance_matrix_theta)), Y) / 2
+    likelihood += n / 2 * np.log(2 * np.pi)
+
+    return likelihood
+
 BETA = 5
 ALPHA = 1
 LENGTHSCALE = 1
@@ -73,6 +87,17 @@ KERNEL_VARIANCE = 1
 PREDICT_SAMPLE_SIZE = 1000
 
 X, Y = loadData("./data/input.data")
+#Gaussian Process
 covariance_matrix = create_covariance_matrix(X, BETA, ALPHA, LENGTHSCALE, KERNEL_VARIANCE)
 mean , variance = GP_predict(X, Y, covariance_matrix, BETA, PREDICT_SAMPLE_SIZE, ALPHA, LENGTHSCALE, KERNEL_VARIANCE)
+GP_plot(X, Y, mean, variance, PREDICT_SAMPLE_SIZE)
+
+#Gaussian Process optimized by maximizing marginal likelihood
+opt = minimize(negative_marginal_log_likelihood, [ALPHA, LENGTHSCALE, KERNEL_VARIANCE], args = (X, Y, BETA))
+alpha_opt, lengthscale_opt, kernel_variance_opt = opt.x
+print('Optimized parameters')
+print(f'alpha: {alpha_opt}, lengthscale: {lengthscale_opt}, kernel_variance: {kernel_variance_opt}')
+
+covariance_matrix = create_covariance_matrix(X, BETA, alpha_opt, lengthscale_opt, kernel_variance_opt)
+mean , variance = GP_predict(X, Y, covariance_matrix, BETA, PREDICT_SAMPLE_SIZE, alpha_opt, lengthscale_opt, kernel_variance_opt)
 GP_plot(X, Y, mean, variance, PREDICT_SAMPLE_SIZE)
