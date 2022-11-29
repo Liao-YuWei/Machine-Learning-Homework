fb46b3d40b2ec03b7a97e3fd1b510b13095d3caf
YuWei Liao <awei6209@gmail.com>
2022-11-29 23:54:45 +0800

    part1 SVM with three different kernels

--- hw5/ML_hw5_SVM.py
+++ hw5/ML_hw5_SVM.py
@@ -1,10 +1,11 @@
 import numpy as np
+from libsvm.svmutil import *
 
 def load_data(x_train_filepath, y_train_filepath, x_test_filepath, y_test_filepath):
     X_train = np.empty((0,784))
-    Y_train = np.empty((0,1))
+    Y_train = []
     X_test = np.empty((0,784))
-    Y_test = np.empty((0,1))
+    Y_test = []
 
     print('Loading training images...')
     with open(x_train_filepath) as f:
@@ -18,7 +19,7 @@ def load_data(x_train_filepath, y_train_filepath, x_test_filepath, y_test_filepa
     with open(y_train_filepath) as f:
         for line in f.readlines():
             label = float(line)
-            Y_train = np.vstack((Y_train, label))
+            Y_train.append(label)
     f.close()
 
     print('Loading test images...')
@@ -33,10 +34,13 @@ def load_data(x_train_filepath, y_train_filepath, x_test_filepath, y_test_filepa
     with open(y_test_filepath) as f:
         for line in f.readlines():
             label = float(line)
-            Y_test = np.vstack((Y_test, label))
+            Y_test.append(label)
     f.close()
 
     return X_train, Y_train, X_test, Y_test
 
 
 X_train, Y_train, X_test, Y_test = load_data('./data/X_train.csv', './data/Y_train.csv', './data/X_test.csv', './data/Y_test.csv')
+for kernel_type in range(3):
+    model = svm_train(Y_train, X_train, f'-t {kernel_type} -q')
+    result = svm_predict(Y_test, X_test, model)
