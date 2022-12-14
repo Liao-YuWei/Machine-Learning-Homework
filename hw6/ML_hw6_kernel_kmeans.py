90c44c5116203e267ef3476b7ba70fe45d7b492f
unknown <awei6209@gmail.com>
2022-12-14 16:28:30 +0800

    kernel kmeans with casual hyperparameters

--- hw6/ML_hw6_kernel_kmeans.py
+++ hw6/ML_hw6_kernel_kmeans.py
@@ -3,6 +3,8 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm, trange
 import os
+import glob
+from scipy.spatial.distance import cdist
 
 def spatial_rbf(gamma, x1, x2):
     x1_row = x1 // 100
@@ -21,17 +23,16 @@ def color_rbf(gamma, x1, x2):
     return math.exp(-gamma * square)
 
 def kernel(img):
-    img_kernel = np.zeros((IMAGE_SIZE, IMAGE_SIZE))
-    progress = tqdm(total=50005000)
-    
-    for row in range(IMAGE_SIZE):
-        for col in range(row, IMAGE_SIZE):
-            rbf_spatial = spatial_rbf(GAMMA_S, row, col)
-            rbf_color = color_rbf(GAMMA_C, img[row], img[col])
-            img_kernel[row][col] = rbf_spatial * rbf_color
-            img_kernel[col][row] = img_kernel[row][col]
-            progress.update(1)
-    
+    color_dist = cdist(img, img, 'sqeuclidean')
+    coordinates = np.zeros((IMAGE_SIZE, 2))
+    for row in range(100):
+        row_index = row * 100
+        for col in range(100):
+            coordinates[row_index + col][0] = row
+            coordinates[row_index + col][1] = col
+    spatial_distance = cdist(coordinates, coordinates, 'sqeuclidean')
+    img_kernel = np.multiply(np.exp(-GAMMA_C * color_dist), np.exp(-GAMMA_S * spatial_distance))
+
     return img_kernel
 
 def construct_C(clusters):
@@ -41,23 +42,29 @@ def construct_C(clusters):
 
     return C
 
-def init_alpha_c(centers):
-    # alpha = np.zeros((IMAGE_SIZE, NUM_CLUSTER), dtype=int)
-    # for i in range(NUM_CLUSTER):
-    #     alpha[centers[i]][i] = 1
-    # clusters = np.sum(alpha, axis=0, dtype=int) #sum up each of column
-
+def init_cluster_c(img_kernel, centers):
     clusters = np.zeros(IMAGE_SIZE, dtype=int)
-    for i in range(NUM_CLUSTER):
-        clusters[centers[i]] = i
+    for pixel in range(IMAGE_SIZE):
+        if pixel in centers:
+            continue
+        min_dist = np.Inf
+        for cluster in range(NUM_CLUSTER):
+            center = centers[cluster]
+            temp_dist = img_kernel[pixel][center]
+            if temp_dist < min_dist:
+                min_dist = temp_dist
+                clusters[pixel] = cluster
+
     C = construct_C(clusters)
+
+    save_picture(clusters, 0)
 
     return clusters, C
 
 def init(img_kernel):
     if MODE == 'random':
         centers = np.random.randint(IMAGE_SIZE, size=NUM_CLUSTER)
-        clusters, C = init_alpha_c(centers)
+        clusters, C = init_cluster_c(img_kernel, centers)
     elif MODE == 'k-means++':
         centers = np.zeros(NUM_CLUSTER, dtype=int)
         centers[0] = np.random.randint(IMAGE_SIZE, size=1)
@@ -73,53 +80,42 @@ def init(img_kernel):
                 distances[j] = min_dist
             distances = distances / np.sum(distances)
             centers[i] = np.random.choice(10000, size=1, p=distances)
-
-        # alpha, clusters = init_alpha_c(centers)
-        clusters, C = init_alpha_c(centers)
+        clusters, C = init_cluster_c(img_kernel, centers)
     else:
         print('Wrong input for cluster initialization!')
 
-    # return alpha, clusters
     return clusters, C
 
-def sigma_n(img_kernel, clusters, j, k):
+def sigma_n(pixel_kernel, clusters, k, C):
     sum = 0
     for n in range(IMAGE_SIZE):
         if clusters[n] == k:
-            sum += img_kernel[j][n]
+            sum += pixel_kernel[n]
     
-    return sum
+    return 2 / C[k] * sum
 
-def sigma_pq(img_kernel, clusters, k):
-    sum = 0
-    group = np.where(clusters == k)
-    print(group)
-    progress = tqdm(total=np.shape(group)[0] * np.shape(group)[0])
-    for p in np.nditer(group):
-        for q in np.nditer(group):
-            sum += img_kernel[p][q]
-            progress.update(1)
-    # for p in range(IMAGE_SIZE):
-    #     for q in range(IMAGE_SIZE):
-    #         if alpha[p][k] == 1 and alpha[q][k] == 1:
-    #             sum += img_kernel[p][q]
-    print('end pq')
+def sigma_pq(img_kernel, clusters, C):
+    sum = np.zeros(NUM_CLUSTER)
+    for k in range(NUM_CLUSTER):
+        ker = img_kernel.copy()
+        for pixel in range(IMAGE_SIZE):
+            if clusters[pixel] != k:
+                ker[pixel, :] = 0
+                ker[:, pixel] = 0
+        sum[k] = np.sum(ker)/ (C[k] **2)
 
     return sum
 
 def kernel_kmeans(img_kernel, clusters, C):
-    # new_alpha = np.zeros((IMAGE_SIZE, NUM_CLUSTER), dtype=int)
     new_clusters = np.zeros(IMAGE_SIZE, dtype=int)
-    for pixel in range(IMAGE_SIZE):
+    pq = sigma_pq(img_kernel, clusters, C)
+    for pixel in trange(IMAGE_SIZE):
         distances = np.zeros(NUM_CLUSTER)
-        for cluster in trange(NUM_CLUSTER):
+        for cluster in range(NUM_CLUSTER):
             distances[cluster] = img_kernel[pixel][pixel]
-            distances[cluster] += 2 / C[cluster] * sigma_n(img_kernel, clusters, pixel, cluster)
-            distances[cluster] += 1 / (C[cluster] ** 2) * sigma_pq(img_kernel, clusters, cluster)
+            distances[cluster] -= sigma_n(img_kernel[pixel, :], clusters, cluster, C)
+            distances[cluster] += pq[cluster]
         new_clusters[pixel] = np.argmin(distances)
-        # cur_cluster = np.argmin(distances)
-        # new_alpha[pixel][cur_cluster] = 1
-    # new_clusters = np.sum(new_alpha, axis=0, dtype=int)
     new_C = construct_C(new_clusters)
         
     return new_clusters, new_C
@@ -137,41 +133,56 @@ def save_picture(clusters, iteration):
         pixel[i, :] = COLOR[clusters[i], :]
     pixel = np.reshape(pixel, (100, 100, 3))
     img = Image.fromarray(np.uint8(pixel))
-    img.save(os.path.join(OUTPUT_DIR, '%06d.png'%iteration))
+    img.save(OUTPUT_DIR + '\%01d_%03d.png'%(NUM_CLUSTER, iteration), 'png')
 
     return
 
+def png_to_gif():
+    frames = []
+    imgs = glob.glob((os.path.join(OUTPUT_DIR, "*.png")))
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+    
+    # Save into a GIF file that loops forever
+    frames[0].save(OUTPUT_DIR + f'\{NUM_CLUSTER}_image{IMAGE_ID}.gif', format='GIF',
+                append_images=frames[1:],
+                save_all=True,
+                duration=300, loop=0)
+    
+
 IMAGE_SIZE = 10000
-GAMMA_S = 1 / IMAGE_SIZE
-GAMMA_C = 1 / (256 ** 3)
-NUM_CLUSTER = 2
-MODE = 'k-means++'
-COLOR = [[56, 207, 0], [64, 70, 230], [186, 7, 61], [245, 179, 66], [55, 240, 240]]
-OUTPUT_DIR = './output/kmeans'
+COLOR = np.array([[56, 207, 0], [64, 70, 230], [186, 7, 61], [245, 179, 66], [55, 240, 240]])
+
+IMAGE_ID = 1
+GAMMA_S = 0.001
+GAMMA_C = 0.001
+NUM_CLUSTER = 4
+MODE = 'random'
+IMAGE_PATH = f'.\data\image{IMAGE_ID}.png'
+OUTPUT_DIR = f'.\output\kmeans\{MODE}\image{IMAGE_ID}'
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
 
 converge = 0
 iteration = 1
 
-img = np.asarray(Image.open('.\data\image1.png').getdata()) #load image to 10000*3 numpy array
+print(f'Image: {IMAGE_ID}, k: {NUM_CLUSTER}, mode: {MODE}')
+img = np.asarray(Image.open(IMAGE_PATH).getdata()) #load image to 10000*3 numpy array
 
 img_kernel = kernel(img)
-# alpha, clusters = init(img_kernel)
 clusters, C = init(img_kernel)
-# print(clusters)
-# print(C)
 
 while not converge:
     print(f'iteration: {iteration}')
     pre_clusters = clusters
 
-    # alpha, clusters = kernel_kmeans(img_kernel, alpha, clusters)
-
-    # converge = check_converge(alpha, pre_alpha)
-    # save_picture(alpha, iteration)
     clusters, C = kernel_kmeans(img_kernel, clusters, C)
-    # print(clusters)
-    # print(C)
 
-    converge = check_converge(clusters, pre_clusters)
     save_picture(clusters, iteration)
+    converge = check_converge(clusters, pre_clusters)
+    
     iteration += 1
+
+png_to_gif()
