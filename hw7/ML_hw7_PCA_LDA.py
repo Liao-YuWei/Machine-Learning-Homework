8b4d5cb515ccbc1571bcdcc9e40d36d6a31513be
unknown <awei6209@gmail.com>
2022-12-27 17:51:18 +0800

    print 25 eigenfaces

--- hw7/ML_hw7_PCA_LDA.py
+++ hw7/ML_hw7_PCA_LDA.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 
 def read_pmg(path):
     with open(path, 'rb') as f:
@@ -29,8 +30,47 @@ def load_imgs(path):
 
     return np.array(image), np.array(filename), np.array(label)
 
+# def standardize(data):
+#     mean = np.mean(data, axis = 0)
+#     std = np.std(data, axis = 0)
+
+#     return (data - mean) / std
+
+def PCA(data):
+    mean = np.mean(data, axis = 0)
+    data_center = data - mean
+
+    covariance = data_center @ data_center.T
+    eigenvalue, eigenvector = np.linalg.eigh(covariance)
+    eigenvector = data_center.T @ eigenvector
+    
+    index = np.argsort(-eigenvalue)
+    eigenvector = eigenvector[:, index]
+
+    W = eigenvector[:, :25]
+    for i in range(W.shape[1]):
+        W[:, i] = W[:, i] / np.linalg.norm(W[:, i])
+
+    return W
+
+def print_eigen_fisher_face(W, face):
+    fig = plt.figure()
+    for i in range(25):
+        img = W[:, i].reshape(231, 195)
+        ax = fig.add_subplot(5, 5, i+1)
+        ax.axis('off')
+        ax.imshow(img, cmap='gray')
+    fig.savefig(f'./output/{face}.jpg')
+    plt.show()
+        
+    return
+
 TRAINING_PATH = './Yale_Face_Database/Training'
 TESTING_PATH = './Yale_Face_Database/Testing'
 
 train_img, train_filename, train_label = load_imgs(TRAINING_PATH)
 test_img, test_filename, test_label = load_imgs(TESTING_PATH)
+
+W = PCA(train_img)
+print_eigen_fisher_face(W, 'eigenface')
+# reconstruct_face(W)
