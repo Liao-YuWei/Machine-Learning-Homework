00cbf7ab49ee77c9357976f853b89260378a44ac
YuWei Liao <awei6209@gmail.com>
2022-12-30 16:36:51 +0800

    rename SNE file

--- hw7/tsne_python/ML_hw7_SNE.py
+++ hw7/tsne_python/ML_hw7_SNE.py
@@ -104,7 +104,7 @@ def pca(X=np.array([]), no_dims=50):
     return Y
 
 
-def tsne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
+def sne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
     """
         Runs t-SNE on the dataset in the NxD array X to reduce its
         dimensionality to no_dims dimensions. The syntaxis of the function is
@@ -181,10 +181,18 @@ def tsne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0):
 
 
 if __name__ == "__main__":
+    mode = int(input('Enter 1 for t-SNE, 2 for symmetric SNE: '))
     print("Run Y = tsne.tsne(X, no_dims, perplexity) to perform t-SNE on your dataset.")
     print("Running example on 2,500 MNIST digits...")
     X = np.loadtxt("mnist2500_X.txt")
     labels = np.loadtxt("mnist2500_labels.txt")
-    Y = tsne(X, 2, 50, 20.0)
+
+    if mode == 1:
+        Y = sne(X, 2, 50, 20.0)
+    elif mode == 2:
+        Y = sne(X, 2, 50, 20.0)
+    else:
+        print('Wrong input of mode!')
+        
     pylab.scatter(Y[:, 0], Y[:, 1], 20, labels)
     pylab.show()
